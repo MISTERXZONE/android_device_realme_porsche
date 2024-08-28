661525d91c066371003a8a9c110d477b22b5fa83
LuK1337 <priv.luk@gmail.com>
2024-08-28 17:06:56 +0200

    porsche: Disable ISunlightEnhancement via soong option
    Change-Id: I6d97d679a08492af06603ebacdb1e9c5ee86b4f2

--- device.mk
+++ device.mk
@@ -46,6 +46,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# LiveDisplay
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
