1afae4c7810d17c84ec0c373ee674c4b71ee9c9d
MISTERXZONE <porschemisterx@gmail.com>
2025-05-17 02:00:35 +0000

    porsche: Let's Welcome Axion
    Signed-off-by: MISTERXZONE <porschemisterx@gmail.com>

--- lineage_porsche.mk
+++ lineage_porsche.mk
@@ -12,15 +12,43 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/porsche/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_porsche
+PRODUCT_NAME := lineage_porsche
 PRODUCT_DEVICE := porsche
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX3312
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Axion Flags
+AXION_MAINTAINER := Mister_X
+
+# Processor name (use "_" for spaces)
+AXION_PROCESSOR := Snapdragon_888
+
+# Define rear camera specs (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,8,2
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 16
+
+# BCR
+TARGET_PREBUILT_BCR := true
+
+# Core groups
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6
+AXION_CPU_PRIME_CORE := 7
+
+# Task assignments
+AXION_CPU_BG := 0,1
+AXION_CPU_FG := 2,3,4
+AXION_CPU_LIMIT_BG := 0
+
+# Wether to enable debugging for adb logcat purposes
+AXION_DEBUGGING_ENABLED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint="realme/RMX3312/RE58B2L1:14/UKQ1.230924.001/S.1a33387-2-4:user/release-keys" \
